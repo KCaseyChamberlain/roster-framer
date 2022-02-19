c613d5e2672e621739a903dde684c17b960dc118
KCaseyChamberlain <caseygchamberlain@gmail.com>
2022-02-19 14:15:23 -0700

    getName, getId, and getEmail are functional. Tests pass

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const promptUser = () => {
             type: 'input',
             name: 'tmname',
             message: "What the team manager’s name? (Required)",
-            validate: tmInput => {
-                if (tmInput) {
+            validate: tmName => {
+                if (tmName) {
                     return true;
                 } else {
                     console.log("Please enter your team manager's name!");
@@ -16,12 +16,39 @@ const promptUser = () => {
                 }
             }
         },
-    ])
-        .then(({ tmname }) => {
-            this.employee = new Employee(tmname);
-            console.log(tmname);
-        });
+        {
+            type: 'input',
+            name: 'tmid',
+            message: "What the team manager’s employee ID? (Required)",
+            validate: tmID => {
+                if (tmID) {
+                    return true;
+                } else {
+                    console.log("Please enter your team manager's employee ID!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'tmemail',
+            message: "What the team manager’s email? (Required)",
+            validate: tmEmail => {
+                if (tmEmail) {
+                    return true;
+                } else {
+                    console.log("Please enter your team manager's email!");
+                    return false;
+                }
+            }
+        },
+    ])        
+    .then(({ tmname, tmid, tmemail }) => {
+        this.employee = new Employee(tmname, tmid, tmemail);
+        console.log(this.employee.getName());
+        console.log(this.employee.getID());
+        console.log(this.employee.getEmail());
+    })
 }
-
 
 promptUser()
