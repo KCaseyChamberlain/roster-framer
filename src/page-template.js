bbab22273fbb72849322743e49b6e782d113963f
KCaseyChamberlain <caseygchamberlain@gmail.com>
2022-02-20 15:30:18 -0700

    generateManager functional and accepts dynamic info

--- src/page-template.js
+++ src/page-template.js
@@ -1,3 +1,4 @@
+const generateTemplate = require('../lib/generate-template.js');
 const cardsHTML = [];
 
 const generatePage = teamInfo => {
@@ -6,15 +7,19 @@ const generatePage = teamInfo => {
         return `
         <div class="card employee-card">
             <div class="title">
+            ${manager.getRole()}
             </div>
             <div class="name">
             ${manager.getName()}
             </div>
             <div class="employee-id">
+            ${manager.getID()}
             </div>
             <div class="email-address">
+            ${manager.getEmail()}
             </div>
             <div class="office-number">
+            ${manager.getOfficeNumber()}
             </div>
         </div>
         `;
@@ -68,10 +73,12 @@ const generatePage = teamInfo => {
         .map(engineer => generateEngineer(engineer))
     );
     console.log(cardsHTML)
-    return cardsHTML.join('')
+    var HTML = generateTemplate(cardsHTML)
+    return HTML
 }
 
 module.exports = generatePage
 
 
 //     ${teamInfo[0].name} 
+// ${manager.getName()}
