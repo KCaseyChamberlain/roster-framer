199338a160838d78bd661ca7f1efdf9861a73605
KCaseyChamberlain <caseygchamberlain@gmail.com>
2022-02-19 22:42:22 -0700

    engineer test issue

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,1 +1,15 @@
 const Engineer = require('../lib/Engineer.js');
+
+test('test create employee object', () => {
+    const engineer = new Engineer('Dave');
+
+    expect(typeof (engineer)).toBe('object')
+});
+
+test('test get engineer Github', () => {
+    const expectedGithub = "jo-gitty"
+    const engineer = new Engineer(expectedGithub);
+
+    var actualGithub = engineer.getGithub()
+    expect(actualGithub).toEqual(expectedGithub)
+});
