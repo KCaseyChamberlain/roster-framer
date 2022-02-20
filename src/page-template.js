9d520b7b7925bda9dea8d3354c524f6df876bf0f
KCaseyChamberlain <caseygchamberlain@gmail.com>
2022-02-20 14:35:37 -0700

    page template generates dynamic employee-cards

--- src/page-template.js
+++ src/page-template.js
@@ -20,6 +20,24 @@ const generatePage = teamInfo => {
         `;
     };
 
+    // create the html for interns
+    const generateIntern = intern => {
+        return `
+        <div class="card employee-card">
+            <div class="title">
+            </div>
+            <div class="name">
+            </div>
+            <div class="employee-id">
+            </div>
+            <div class="email-address">
+            </div>
+            <div class="school">
+            </div>
+        </div>
+        `;
+    };
+
      // create the html for engineers
     const generateEngineer = engineer => {
         return `
@@ -37,40 +55,20 @@ const generatePage = teamInfo => {
         </div>
         `;
     };
-
-    // create the html for interns
-    const generateIntern = intern => {
-        return `
-        <div class="card employee-card">
-            <div class="title">
-            </div>
-            <div class="name">
-            </div>
-            <div class="employee-id">
-            </div>
-            <div class="email-address">
-            </div>
-            <div class="github-username">
-            </div>
-        </div>
-        `;
-    };
     cardsHTML.push(teamInfo
-        .filter(employee => employee.getRole() === "Manager")
+        .filter(employee => employee.getRole() === "Team Manager")
         .map(manager => generateManager(manager))
-    );
-    cardsHTML.push(teamInfo
-        .filter(employee => employee.getRole() === "Engineer")
-        .map(engineer => generateEngineer(engineer))
     );
     cardsHTML.push(teamInfo
         .filter(employee => employee.getRole() === "Intern")
         .map(intern => generateIntern(intern))
+    )
+    cardsHTML.push(teamInfo
+        .filter(employee => employee.getRole() === "Engineer")
+        .map(engineer => generateEngineer(engineer))
     );
-    return cardsHTML.join(''),
     console.log(cardsHTML)
-    // cardsHTML.flat()
-    // console.log(cardsHTML.flat());
+    return cardsHTML.join('')
 }
 
 module.exports = generatePage
