7299e4810d2926a4cbf4a03729fbea3b5741e3e6
KCaseyChamberlain <caseygchamberlain@gmail.com>
2022-02-20 04:58:04 -0700

    start of html text created, generateManagerEngineerIntern cards in place

--- src/page-template.js
+++ src/page-template.js
@@ -1,26 +1,80 @@
-function generatePage(teamInfo){
-    return `
-    Manager's Info:
-    ${teamInfo[0].name} 
-    ${teamInfo[0].id} 
-    ${teamInfo[0].email} 
-    ${teamInfo[0].title} 
-    ${teamInfo[0].tmOfficeNumber}
-    
-    Intern's Info:
-    ${teamInfo[1].name}
-    ${teamInfo[1].id}
-    ${teamInfo[1].email}
-    ${teamInfo[1].title}
-    ${teamInfo[1].inSchool}
+const cardsHTML = [];
 
-    Engineer's Info:
-    ${teamInfo[2].name}
-    ${teamInfo[2].id}
-    ${teamInfo[2].email}
-    ${teamInfo[2].title}
-    ${teamInfo[2].enGithub}
-    `
+const generatePage = teamInfo => {
+    const generateManager = manager => {
+        return `
+        <div class="card employee-card">
+            <div class="title">
+            </div>
+            <div class="name">
+            </div>
+            <div class="employee-id">
+            </div>
+            <div class="email-address">
+            </div>
+            <div class="office-number">
+            </div>
+        </div>
+        `;
+    };
+
+    const generateEngineer = engineer => {
+        return `
+        <div class="card employee-card">
+            <div class="title">
+            </div>
+            <div class="name">
+            </div>
+            <div class="employee-id">
+            </div>
+            <div class="email-address">
+            </div>
+            <div class="github-username">
+            </div>
+        </div>
+        `;
+    };
+
+    // create the html for interns
+    const generateIntern = intern => {
+        return `
+        <div class="card employee-card">
+            <div class="title">
+            </div>
+            <div class="name">
+            </div>
+            <div class="employee-id">
+            </div>
+            <div class="email-address">
+            </div>
+            <div class="github-username">
+            </div>
+        </div>
+        `;
+    };
+    cardsHTML.push(teamInfo
+        .filter(employee => employee.getRole() === "Manager")
+        .map(manager => generateManager(manager))
+    );
+    cardsHTML.push(teamInfo
+        .filter(employee => employee.getRole() === "Engineer")
+        .map(engineer => generateEngineer(engineer))
+    );
+    cardsHTML.push(teamInfo
+        .filter(employee => employee.getRole() === "Intern")
+        .map(intern => generateIntern(intern))
+    );
 }
 
 module.exports = generatePage
+
+// function generatePage(teamInfo) {
+//     return `
+//     Manager's Info:
+//     ${teamInfo[0].name} 
+//     ${teamInfo[0].id} 
+//     ${teamInfo[0].email} 
+//     ${teamInfo[0].title} 
+//     ${teamInfo[0].tmOfficeNumber}
+//     `
+// }
