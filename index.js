64340375b6699c5bcec5cff220229e764cbd27ed
KCaseyChamberlain <caseygchamberlain@gmail.com>
2022-02-19 21:04:10 -0700

    areWeDone

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const inquirer = require('inquirer');
 const Employee = require('./lib/Employee.js');
 // const Engineer = require('./lib/Engineer.js');
-// const Intern = require('./lib/Intern.js');
+const Intern = require('./lib/Intern.js');
 const Manager = require('./lib/Manager.js');
 const Questions = require('./lib/questions.js')
-
 teamInfo = []
 
 function addManager() {
@@ -15,6 +14,22 @@ function addManager() {
             teamInfo.push(manager)
             console.log(manager.name + 'Has been added to the roster')
             console.log(teamInfo)
+            areWeDone()
+        })
+}
+
+function areWeDone() {
+    inquirer
+        .prompt(Questions.areWeDoneQuestions)
+        .then(function (answers){
+            console.log(answers)
+            if(answers = 'Add intern to roster.'){
+                console.log("Add intern")
+            } else if (answers = 'Add engineer to roster.') {
+                console.log("Add engineer")
+            } else{
+                console.log("Finished")
+            }
         })
 }
 
